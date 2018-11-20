96f73dc14779ed85d3ca16f4c99d887b09fd997b
Peter Takacs <peter.takacs@koodilab.com>
2018-11-20 21:37:19 +0100

    Added the dotenv package

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/petaki/probe
+
+require github.com/joho/godotenv v1.3.0
--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main // import "github.com/petaki/probe"
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	fmt.Println("Starting Probe.")
+
+	redisPrefix := os.Getenv("REDIS_PREFIX")
+	redisHost := os.Getenv("REDIS_HOST")
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+	redisPort := os.Getenv("REDIS_PORT")
+	redisDb := os.Getenv("REDIS_DB")
+
+	fmt.Println(redisPrefix, redisHost, redisPassword, redisPort, redisDb)
 }
