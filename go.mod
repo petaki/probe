6487031c6fd23ee6ee3e457072b4344bdb730416
Peter Takacs <peter.takacs@koodilab.com>
2018-11-20 22:43:24 +0100

    Added the current system information

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/petaki/probe
 
-require github.com/joho/godotenv v1.3.0
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/shirou/gopsutil v2.18.10+incompatible
+	golang.org/x/sys v0.0.0-20181119195503-ec83556a53fe // indirect
+)
--- main.go
+++ main.go
@@ -4,14 +4,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/mem"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal(err)
 	}
 
 	fmt.Println("Starting Probe.")
@@ -23,4 +27,32 @@ func main() {
 	redisDb := os.Getenv("REDIS_DB")
 
 	fmt.Println(redisPrefix, redisHost, redisPassword, redisPort, redisDb)
+
+	cpuPercent, err := cpu.Percent(3*time.Second, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(cpuPercent[0])
+
+	virtualMemory, err := mem.VirtualMemory()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(virtualMemory.UsedPercent)
+
+	partitions, err := disk.Partitions(false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, parititon := range partitions {
+		usage, err := disk.Usage(parititon.Mountpoint)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(parititon.Mountpoint, usage.UsedPercent)
+	}
 }
